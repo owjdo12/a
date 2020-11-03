01f6640de9368d5c2fb6d6ddc3c8db5be805aa81
yeji <owjdo@naver.com>
2020-11-03 15:15:32 +0900

    오류제거

--- 10.Exception/src/exception/ExceptionTest2.java
+++ 10.Exception/src/exception/ExceptionTest2.java
@@ -3,9 +3,8 @@ package exception;
 public class ExceptionTest2 {
 	public static void main(String[] args) {
 		String data1 = "100";
-		String data2 = "a100";
+		String data2 = "200";
 		
-		//런타임에러
 		int value1 = Integer.parseInt(data1);
 		int value2 = Integer.parseInt(data2);
 		
