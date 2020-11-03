a3d136a27cb8d4ed74b78d21de22c4820fff6bef
yeji <owjdo12@naver.com>
2020-11-03 18:10:44 +0900

    커밋수정

--- 10.Exception/src/exception/ExceptionTest2.java
+++ 10.Exception/src/exception/ExceptionTest2.java
@@ -3,13 +3,12 @@ package exception;
 public class ExceptionTest2 {
 	public static void main(String[] args) {
 		String data1 = "100";
-		String data2 = "a100";
-		
-		//·±Å¸ÀÓ¿¡·¯
+		String data2 = "200";
+
 		int value1 = Integer.parseInt(data1);
 		int value2 = Integer.parseInt(data2);
 		
 		System.out.println(value1 + value2);
-		system.out.println(value1);
+		System.out.println(value1);
 	}
 }
