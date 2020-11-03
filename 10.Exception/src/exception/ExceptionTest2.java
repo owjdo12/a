82de11092dc35a83eabdcc7f5d89f738b167a4c1
yeji <owjdo@naver.com>
2020-11-03 15:01:26 +0900

    소스 수정

--- 10.Exception/src/exception/ExceptionTest2.java
+++ 10.Exception/src/exception/ExceptionTest2.java
@@ -1,21 +1,14 @@
 package exception;
-/*
- * Error(해결못함 -컴퓨터다운..)
- * Exception(예외)
- * 컴파일 예외 - 코딩할 때 이클립스에서 알려줌
- * 런타임 예외 - 실행 할 때 나타나는 예외사항
- */
+
 public class ExceptionTest2 {
-
 	public static void main(String[] args) {
-		//컴파일 예외
-		//Class.forName("java.lang.String2");
-		try {
-			Class.forName("java.lang.String2");
-		} catch (ClassNotFoundException e) {
-			//e.printStackTrace();
-			System.out.println("입력값을 확인해 주세요");
-		}
+		String data1 = "100";
+		String data2 = "a100";
+		
+		//런타임에러
+		int value1 = Integer.parseInt(data1);
+		int value2 = Integer.parseInt(data2);
+		
+		System.out.println(value1 + value2);
 	}
-
 }
